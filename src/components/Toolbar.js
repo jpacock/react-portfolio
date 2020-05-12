0c7fb929b5d065f38e5e8fcfae8b52e530f27179
jpacock <jpacock@us.ibm.com>
2020-05-12 12:39:24 -0500

    add mailto link

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { GrGithub, GrInstagram, GrLinkedin } from "react-icons/gr";
+import { AiOutlineMail } from "react-icons/ai"
 
 import { Link } from 'rebass';
 
@@ -15,6 +16,7 @@ const Toolbar = () => {
                     <div className="profile-job">IBM Coud Lab Services</div>
                     <hr className="horizontal-rule" />
                     <div >
+                        <Link href="mailto:jpacock@us.ibm.com"><AiOutlineMail className="contact-icon" /></Link>
                         <Link href="https://www.linkedin.com/in/jordan-acock-8ba91152/"><GrLinkedin className="contact-icon" /></Link>
                         <Link href="https://www.github.com/jpacock"><GrGithub className="contact-icon" /></Link>
                         <Link href="https://www.instagram.com/jordan.acock"><GrInstagram className="contact-icon" /></Link>
